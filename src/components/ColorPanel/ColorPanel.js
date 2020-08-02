91c668dccec4cc2c3f8107ecf544b9005b3099e3
Tôn <nghuuton@gmail.com>
2020-08-02 18:43:21 +0700

    init app

---
+++ src/components/ColorPanel/ColorPanel.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ColorPanel extends Component {
+    state = {};
+    render() {
+        return <div>colorpanel</div>;
+    }
+}
+
+export default ColorPanel;
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SidePanel extends Component {
+    state = {};
+    render() {
+        return <div>sidepanel</div>;
+    }
+}
+
+export default SidePanel;
