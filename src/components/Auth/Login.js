6249c0c2227539353d04f9433443454bf4a12978
Tôn <nghuuton@gmail.com>
2020-08-02 17:40:41 +0700

    Login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,114 @@
 import React, { Component } from "react";
+import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import md5 from "md5";
+
+import firebase from "../../firebase";
 
 class Login extends Component {
+    state = {
+        email: "",
+        password: "",
+        errors: [],
+        loading: false,
+    };
+    /**
+     * @param {event} event
+     */
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+    /**
+     * @param {event} event
+     */
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (this.isFormValid(this.state)) {
+            this.setState({ errors: [], loading: true });
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then((sigInUser) => {
+                    console.log(sigInUser);
+                })
+                .catch((err) => {
+                    console.error(err);
+                    this.setState({
+                        errors: this.state.errors.concat(err),
+                        loading: false,
+                    });
+                });
+        }
+    };
+
+    isFormValid = ({ email, password }) => email && password;
+
+    handleInputError = (errors, inputName) =>
+        errors.some((error) => error.message.toLowerCase().includes(inputName))
+            ? "error"
+            : "";
+
     render() {
-        return <div>Login</div>;
+        const { email, password, errors, loading } = this.state;
+        return (
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h1" icon color="violet" textAlign="center">
+                        <Icon name="code branch" color="violet" />
+                        Login for DevChat
+                    </Header>
+                    <Form size="large" onSubmit={this.handleSubmit}>
+                        <Segment stacked>
+                            <Form.Input
+                                fluid
+                                name="email"
+                                icon="mail"
+                                iconPosition="left"
+                                placeholder="Email Address"
+                                onChange={this.handleChange}
+                                type="email"
+                                className={this.handleInputError(errors, "email")}
+                                value={email}
+                            />
+                            <Form.Input
+                                fluid
+                                name="password"
+                                icon="lock"
+                                iconPosition="left"
+                                placeholder="Password"
+                                onChange={this.handleChange}
+                                type="password"
+                                className={this.handleInputError(errors, "password")}
+                                email={password}
+                            />
+                            <Button
+                                className={loading ? "loading" : ""}
+                                disabled={loading}
+                                color="violet"
+                                fluid
+                                size="large"
+                            >
+                                Login
+                            </Button>
+                        </Segment>
+                    </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
+                    <Message>
+                        Don't have account? <Link to="/register">Register</Link>
+                    </Message>
+                </Grid.Column>
+            </Grid>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
+import firebase from "./firebase";
+
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </BrowserRouter>
-);
+class Root extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
 
 ReactDOM.render(<Root />, document.getElementById("root"));
