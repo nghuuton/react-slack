fc6e10ad7bd23f46ad071537fedd1efc4aadec2e
Tôn <nghuuton@gmail.com>
2020-08-03 19:27:21 +0700

    Add file upload

--- src/components/Messages/FileModal.js
+++ src/components/Messages/FileModal.js
@@ -1,18 +1,59 @@
 import React, { Component } from "react";
 import { Modal, Input, Button, Icon } from "semantic-ui-react";
-
+import mime from "mime-types";
 class FileModal extends Component {
-    state = {};
+    state = {
+        file: null,
+        authorized: ["image/jpeg", "image/png"],
+    };
+
+    /**
+     *
+     * @param {event} event
+     */
+    addFile = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            this.setState({ file });
+        }
+    };
+
+    sendFile = () => {
+        const { file } = this.state;
+        const { uploadFile, closeModal } = this.props;
+        if (file !== null) {
+            if (this.isAuthorized(file.name)) {
+                // send file
+                const metadata = { contentType: mime.lookup(file.name) };
+                uploadFile(file, metadata);
+                closeModal();
+                this.clearFile();
+            }
+        }
+    };
+
+    clearFile = () => {
+        this.setState({ file: null });
+    };
+
+    isAuthorized = (fileName) => this.state.authorized.includes(mime.lookup(fileName));
+
     render() {
         const { modal, closeModal } = this.props;
         return (
-            <Modal basic open={modal} onClick={closeModal}>
+            <Modal basic open={modal}>
                 <Modal.Header>Select an Image File</Modal.Header>
                 <Modal.Content>
-                    <Input fluid label="File types: jpg, png" name="file" type="file" />
+                    <Input
+                        onChange={this.addFile}
+                        fluid
+                        label="File types: jpg, png"
+                        name="file"
+                        type="file"
+                    />
                 </Modal.Content>
                 <Modal.Actions>
-                    <Button color="green" inverted>
+                    <Button onClick={this.sendFile} color="green" inverted>
                         <Icon name="checkmark" /> Send
                     </Button>
                     <Button color="red" inverted onClick={closeModal}>
